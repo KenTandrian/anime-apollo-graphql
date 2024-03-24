cdef41a18335240f1210b130b7bdd0dd1538f944
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2024-03-24 15:30:56 +0000

    feat(vite): replace Babel with SWC

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
